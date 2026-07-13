b87158dc94a97362733f0ec4954d7c8dbb4e56ce
chandanjonson <51283964+chandanjonson@users.noreply.github.com>
2026-07-13 08:21:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (36.36%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -2,13 +2,12 @@ class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
         //here brute force will give tle 
         List<Integer> ans = new ArrayList<>();
-        var str = String.valueOf(123456789);
+        var str = "123456789";
         for(int i=2;i <=9;i++){
-          for(int j=0;j <= 9-i;j++){
-            var val = str.substring(j,j+i);
+          for(int j=0;j <=9-i;j++){
+            var val = str.substring(j,(j+i));
             if(!val.isEmpty()){
              var intVlaue = Integer.parseInt(val);
-                
                 if(intVlaue >= low && intVlaue <= high){
                 ans.add(intVlaue);
                 }
